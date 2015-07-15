75d0d782cd3f7b33eabf6f2c7cf2926e9550d6f0
Diarmuid <diarmuid.simpson@cigna.com>
2015-07-15 16:22:16 +0100

    Updated give us a call cta block mark-up

--- data/drupal7/sites/all/themes/cignaGlobal/functions/misc.php
+++ data/drupal7/sites/all/themes/cignaGlobal/functions/misc.php
@@ -45,3 +45,23 @@ function cignaIndonesia_breadcrumb($variables) {
         return $crumbs;
     }
 }
+
+/**
+ * Implements theme_double_field().
+ */
+function cignaGlobal_double_field($vars) {
+    $element = $vars['element'];
+    $settings = $element['#display']['settings'];
+
+    if ($settings['style'] == 'link') {
+        $output = l($element['#item']['first'], $element['#item']['second']);
+    }
+    elseif ($settings['style'] == 'simple') {
+        $output = $element['#item']['first'] . $element['#item']['second'];
+    }
+    else {
+        $output = $element['#item']['first'];
+        $output .= '<strong>' . $element['#item']['second'] . '</strong><br/>';
+    }
+    return $output;
+}
